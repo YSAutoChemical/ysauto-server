8fc009674f984b3584dc8b51d7f89a9165bf72a7
sorious77 <sorious77@naver.com>
2024-03-05 22:36:41 +0900

    ktlint 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
-group = "com.hoang"
+group = "com.ys"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -23,6 +24,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.3")
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("com.google.firebase:firebase-admin:9.2.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
